739e50d379584cf279f6b4110a17d8feb70283ac
jumpnlife <jumpn.life@gmail.com>
2022-11-28 19:58:10 +0800

    11281958

--- App.js
+++ App.js
@@ -3,14 +3,12 @@ import {NativeBaseProvider} from 'native-base';
 import TabNavi from './components/navigator/TabNavi';
 import {Provider} from 'react-redux';
 import {store} from './components/redux/store';
-import {Counter} from './components/redux/counter/Counter';
 
 export default function App() {
   return (
     <Provider store={store}>
       <NativeBaseProvider>
         <TabNavi />
-        {/* <Counter /> */}
       </NativeBaseProvider>
     </Provider>
   );
--- components/redux/store.js
+++ components/redux/store.js
@@ -1,7 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from './counter/counterSlice';
 
-export const store = configureStore({
+export default store = configureStore({
   reducer: {
     counter: counterReducer,
   },
